fc5329f2f5b85c454f83907e9e37d7153bafea47
sautkina <sautkina@ispras.ru>
2021-10-29 17:36:06 +0300

    add new RK methods

--- src/TimeStepper/RungeKutta.cpp
+++ src/TimeStepper/RungeKutta.cpp
@@ -1,17 +1,23 @@
 #include "RungeKutta.h"
-
+ 
 using namespace std;
 
 RungeKutta::RungeKutta(int o,  Basis& b, Solver& s, Solution& ss, Limiter& l, TimeControl& t) : TimeStepper(o, b, s, ss, l, t)
 {
     // Initialization of the parameters
     nStages = order; // OK for RK order <= 4
+    
+    if (order < 0) // just for modifications
+        nStages = 3;
+
+    if (order < -2) // just for new modifications
+        nStages = 4;
 
     // Cfts arrays sizing 
-    alpha.resize(nStages);
+    alpha.resize(nStages, 0.0);
     beta.resize(nStages);
     for (int i = 0; i < nStages; ++i)
-        beta[i].resize(nStages);
+        beta[i].resize(nStages, 0.0);
 
     // Initialization of the RK-cfts
     setButcherTable();
@@ -43,18 +49,120 @@ void RungeKutta::setButcherTable()
             break;
 
         case 3:
-
-            alpha[1] = 0.5;
+        
             alpha[2] = 1.0;
 
-            beta[0][0] = 0.5;
-            beta[1][1] = 1.0;
+            beta[0][0] = 1.0;
+            beta[1][0] = 0.25;
+            beta[1][1] = 0.25;
 
             beta[2][0] = 0.1666666666666667;
-            beta[2][1] = 0.6666666666666667;
-            beta[2][2] = 0.1666666666666667;
+            beta[2][1] = 0.1666666666666667;
+            beta[2][2] = 0.6666666666666667;
+            
+            break;
+
+        case -1:
+	    
+	    alpha[2] = 1.0;
+
+            beta[0][0] = 0.5;
+            
+            beta[1][0] = 0.5;
+            beta[1][1] = 0.5;
+
+            beta[2][0] = 0.3333333333333333;
+            beta[2][1] = 0.3333333333333333;
+            beta[2][2] = 0.3333333333333333;
+            
+            break;
+            
+        case -2:
+
+            alpha[2] = 1.0;
+
+            beta[0][0] = 0.3333333333333333;
+            
+            beta[1][0] = 0.3333333333333333;
+            beta[1][1] = 0.3333333333333333;
+
+            beta[2][0] = 0.25;
+            beta[2][1] = 0.0;
+            beta[2][2] = 0.75;
 
             break;
+        
+        case -3:
+
+            alpha[3] = 1.0;
+
+            beta[0][0] = 0.3333333333333333;
+            
+            beta[1][0] = 0.3333333333333333;
+            beta[1][1] = 0.3333333333333333;
+
+            beta[2][0] = 0.3333333333333333;
+            beta[2][1] = 0.3333333333333333;
+            beta[2][2] = 0.3333333333333333;
+            
+            beta[3][0] = 0.25;
+            beta[3][1] = 0.25;
+            beta[3][2] = 0.25;
+            beta[3][3] = 0.25;
+
+            break;
+            
+        case -4:
+        {
+            alpha[3] = 1.0;
+            
+            double pp = 0.6;
+            double aa = 6.0/11.0;
+            double bb = 56.0/111.0;
+            double gg = 31.0/216.0;
+
+            beta[0][0] = pp;
+            
+            beta[1][0] = pp * (1.0 - aa);
+            beta[1][1] = pp * (1.0 - aa);
+
+            beta[2][0] = pp * (1.0 - aa) * (1.0 - bb);
+            beta[2][1] = pp * (1.0 - aa) * (1.0 - bb);
+            beta[2][2] = pp * (1.0 - bb);
+
+            beta[3][0] = pp * (1.0 - aa) * (1.0 - bb) * (1.0 - gg);
+            beta[3][1] = pp * (1.0 - aa) * (1.0 - bb) * (1.0 - gg);
+            beta[3][2] = pp * (1.0 - bb) * (1.0 - gg);
+            beta[3][3] = pp * (1.0 - gg);
+
+            break;
+        }
+
+        case -5:
+        {
+            alpha[3] = 1.0;
+            
+            double pp = 0.528;//0.57132;
+            double aa = 0.2514970059880235;//0.4611604448659367;
+            double bb = 0.601766535035007;//0.5334643755038199;
+            double gg = 0.05036722250048653;//0.11119560206959966;
+
+            beta[0][0] = pp;
+            
+            beta[1][0] = pp * (1.0 - aa);
+            beta[1][1] = pp * (1.0 - aa);
+
+            beta[2][0] = pp * (1.0 - aa) * (1.0 - bb);
+            beta[2][1] = pp * (1.0 - aa) * (1.0 - bb);
+            beta[2][2] = pp * (1.0 - bb);
+
+            beta[3][0] = pp * (1.0 - aa) * (1.0 - bb) * (1.0 - gg);
+            beta[3][1] = pp * (1.0 - aa) * (1.0 - bb) * (1.0 - gg);
+            beta[3][2] = pp * (1.0 - bb) * (1.0 - gg);
+            beta[3][3] = pp * (1.0 - gg);
+
+            break;
+        }
 
         case 4:
 
@@ -81,6 +189,8 @@ void RungeKutta::setButcherTable()
                  << order \
                  << "is not implemented. " \
                  << "Please change order to 1, 2, 3 or 4." \
+                 << "Use order -1 for RK2BC1 or -2 for RK2BC2." \
+                 << "Use order -3 for RK4ORD2 or -4 for RK4ORD3." \
                  << endl;
             exit(1);
     }
@@ -115,7 +225,6 @@ void RungeKutta::Tstep()
     /// The very step of the RK method
     for (int iStage = 0; iStage < nStages; ++iStage)
     {
-
         T.updateTimeValueRK(alpha[iStage]*tau);   // ??? Is it necessary?
 
         // MPI exchange between neib procs
@@ -133,13 +242,12 @@ void RungeKutta::Tstep()
          //        cout << p << ' ' << sln.SOL[p] << endl;
          // }
 
-        // assemble rhs
+        // assemble rhs 
         t0 = MPI_Wtime();
         k[iStage] = slv.assembleRHS(sln.SOL);
         t1 = MPI_Wtime();
         if (debug) logger << "\tslv.assembleRHS(): " << t1 - t0 << endl;
 
-        //cout << "RHS OK" << endl;
 
         // if (myRank == 0)
         // {
